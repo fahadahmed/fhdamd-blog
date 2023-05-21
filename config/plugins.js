00faf84be66be5f5ecdb06c36c6f19b56e8618a2
Fahad Ahmed <fahad.ahmed@me.com>
2023-05-21 16:26:11 +1000

    updated strapi to include graphql endpoint

--- config/plugins.js
+++ config/plugins.js
@@ -13,4 +13,16 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
 });
