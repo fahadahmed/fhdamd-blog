4df92567ecb8b9a0e0323027e7b7fe0342abb118
Fahad Ahmed <fahad.ahmed@me.com>
2023-05-21 17:56:25 +1000

    updated graphql config

--- config/plugins.js
+++ config/plugins.js
@@ -18,8 +18,6 @@ module.exports = ({ env }) => ({
       endpoint: "/graphql",
       shadowCRUD: true,
       playgroundAlways: true,
-      depthLimit: 7,
-      amountLimit: 100,
       apolloServer: {
         tracing: true,
         introspection: true,
